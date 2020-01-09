02df3d6f9532e4567df6ceb651493eae4c25b279
Jonathan Foucher <jfoucher@gmail.com>
2020-01-09 12:27:25 +0100

    Upgrade webpack encore

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var Encore = require('@symfony/webpack-encore');
 Encore
   .setOutputPath('public/builds/')
   .setPublicPath('/builds')
-
+    .disableSingleRuntimeChunk()
   // this will be your app!
   .addEntry('app', './assets/js/app.js')
   .autoProvidejQuery()
