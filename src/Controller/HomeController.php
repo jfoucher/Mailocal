e4d7938da50e99b5299ae6ff0035e37e4c6964c6
Richard <richardsjoqvist@gmail.com>
2020-09-10 11:14:46 +0200

    Remove filter it does not work anyway

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -85,15 +85,15 @@ class HomeController extends AbstractController
          */
         $repository = $this->getDoctrine()->getRepository(Email::class);
         $criteria = [];
-        if(!$filter) {
-            $filter = $_GET['s'] ?? null;
-        }
-        if ($filter) {
-            $criteria['to = ?'] = $filter;
-            $criteria['subject = ?'] = $filter;
-            $criteria['text = ?'] = $filter;
-            $criteria['html = ?'] = $filter;
-        }
+//        if(!$filter) {
+//            $filter = $_GET['s'] ?? null;
+//        }
+//        if ($filter) {
+//            $criteria['to = ?'] = $filter;
+//            $criteria['subject = ?'] = $filter;
+//            $criteria['text = ?'] = $filter;
+//            $criteria['html = ?'] = $filter;
+//        }
 
         $emails = $repository->allOrderedDateDesc($criteria, 'or');
 
